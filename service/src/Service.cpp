b0b4d1d2e0a2d4df7d375b170400d6a810cb1098
Martinez, Laura <laura.martinez4@univ-tlse3.fr>
2020-12-03 14:14:34 +0100

    Added calcul and display to Service

--- service/src/Service.cpp
+++ service/src/Service.cpp
@@ -10,43 +10,92 @@
 using namespace std;
 
 class Service {
-    int width, height;
-  public:
-    int saveMemory(int);
-    int loadMemory(int);
+    public:
+        int saveMemory(int x);
+        list<int> loadMemory(int n);
+        int calculOutput(list<int> list);
+        void display(int result);
+
+    private:
+        list<int> storage;
 };
 
 int Service::saveMemory (int x) {
     ofstream memory;
-    memory.open("data/memory");
-    memory << x;
-    memory.close();
-    width = x;
-}
+    string valueToSave;
 
-int loadMemory(int n){
-    ifstream memory;
-    memory.open("data/memory");
+    valueToSave = "\n" + std::to_string(x);
+
+    memory.open("service/data/memory", fstream::app);
     
-    string line;
-
-    list<int> x (n);
     if(memory.is_open()){
-        while ( getline(memory,line) )
-        {
-            cout << line << '\n';
-        }
+        memory << valueToSave;
         memory.close();
+        return 1;
     }
+    else
+    {
+        cout << "Le fichier n'a pas pu être ouvert!" << '\n';
+        return -1;
+    }
+    
+
+
     return 0;
 }
 
+list<int> Service::loadMemory(int n){
+    ifstream memory ("service/data/memory");
+    string line;
+
+    list<int> valueLoaded;
+    list<int>::iterator it = valueLoaded.begin();
+
+    int i =0;
+
+    if(memory.is_open()){
+        while ( getline(memory,line) && i != n)
+        {
+            i++;
+            int value = stoi(line);
+            valueLoaded.push_back(value);
+            it++;
+            cout << *it << '\n';
+
+        }
+        memory.close();
+    }
+    else
+    {
+        cout << "Le fichier n'a pas pu être ouvert!" << '\n';
+        storage.push_back(-1);
+    }
+
+    return valueLoaded;
+}
+
+int Service::calculOutput(list<int> list) {
+    int i = 0;
+    for(int nb : list) {
+        if(i == list.size()/2) {
+            return nb;
+        }
+        i++;
+    }
+}
+
+void Service::display(int result) {
+    cout << "Result : " << result << "\n";
+}
 
 int main(int argc, char const *argv[])
 {
 	Service srv;
     int n = 5;
-    srv.loadMemory(n);
+    srv.saveMemory(n);
+    list<int> l = srv.loadMemory(n);
+    int r = srv.calculOutput(l);
+    srv.display(r);
 	
     return 0;
 }
