b9e5ead7c2a49ab71f3eb718ed6cf703cb857463
CADEL <sylvain.cadel@univ-tlse3.fr>
2020-12-03 11:43:49 +0100

    Save and load memory

--- service/src/Service.cpp
+++ service/src/Service.cpp
@@ -10,42 +10,71 @@
 using namespace std;
 
 class Service {
-    int width, height;
   public:
-    int saveMemory(int);
-    int loadMemory(int);
+    int saveMemory(int x);
+    list<int> loadMemory(int n);
 };
 
 int Service::saveMemory (int x) {
     ofstream memory;
-    memory.open("data/memory");
-    memory << x;
-    memory.close();
-    width = x;
+    string valueToSave;
+
+    valueToSave = "\n" + std::to_string(x);
+
+    memory.open("service/data/memory", fstream::app);
+    
+    if(memory.is_open()){
+        memory << valueToSave;
+        memory.close();
+        return 1;
+    }
+    else
+    {
+        cout << "Le fichier n'a pas pu être ouvert!" << '\n';
+        return -1;
+    }
+    
+
+
+    return 0;
 }
 
-int loadMemory(int n){
-    ifstream memory;
-    memory.open("data/memory");
-    
+list<int> Service::loadMemory(int n){
+    ifstream memory ("service/data/memory");
     string line;
 
-    list<int> x (n);
+    list<int> valueLoaded;
+    list<int>::iterator it = valueLoaded.begin();
+
+    int i =0;
+
     if(memory.is_open()){
-        while ( getline(memory,line) )
+        while ( getline(memory,line) && i != n)
         {
-            cout << line << '\n';
+            i++;
+            int value = stoi(line);
+            valueLoaded.push_back(value);
+            it++;
+            cout << *it << '\n';
+
         }
         memory.close();
     }
-    return 0;
+    else
+    {
+        cout << "Le fichier n'a pas pu être ouvert!" << '\n';
+        valueLoaded.push_back(-1);
+    }
+    
+    return valueLoaded;
 }
 
 
 int main(int argc, char const *argv[])
 {
 	Service srv;
-    int n = 5;
+    int n = 11;
+    srv.saveMemory(n);
     srv.loadMemory(n);
 	
     return 0;
