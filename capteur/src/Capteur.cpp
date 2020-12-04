18cb265b32e1939f6a45a8712fb8d4198c626b62
Firerazzer <munster31700@gmail.com>
2020-12-04 17:18:14 +0100

    Capteur Broadcast UDP OK + nouveau tasks.json dans example.vscode

--- capteur/src/Capteur.cpp
+++ capteur/src/Capteur.cpp
@@ -3,16 +3,31 @@
 #include <stdlib.h>
 #include <ctime>
 #include <unistd.h>
+#include <sys/select.h>
+#include <thread>
+
+#include "Server.hpp"
 
 using namespace std;
+string key = "";
+
+void keyPressed() {
+	cout << "press enter to stop the captor" << endl;
+	cin.get();
+	key = "q";
+}
 
 int main(int argc, char const *argv[])
 {
-	while (true)
+	Server s;
+	s.start();
+	thread first (keyPressed);
+	while (key.compare(""))
 	{
-		printf("%d\n", rand() %100);
+		s.sendToAll(to_string((rand()%100)).c_str(), 6);
 		sleep(1);
 	}
-	
+	first.join();
+	s.stop();
     return 0;
 }
--- capteur/src/Server.cpp
+++ capteur/src/Server.cpp
@@ -1,89 +1,57 @@
-#include "Server.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include "Server.hpp"
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <arpa/inet.h>
+  
+using namespace std;
+
+#define PORT     8080 
+#define MAXLINE 1024 
+
+// Driver code 
+void Server::sendToAll(const char* data, unsigned int len) { 
+    sendto(sockfd, data, len, MSG_CONFIRM, (struct sockaddr*) &cliaddr, sizeof cliaddr);
+} 
+
+
+bool Server::start() {
+    struct sockaddr_in servaddr; 
+      
+    // Creating socket file descriptor 
+    if ( (this->sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&servaddr, 0, sizeof(servaddr));
+      
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
+      
+    // Bind the socket with the server address 
+    if ( bind(this->sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    memset(&cliaddr, 0, sizeof cliaddr);
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(PORT);
+    // broadcasting address for unix (?)
+    inet_aton("127.0.0.255", &cliaddr.sin_addr);
+    //cliaddr.sin_addr.s_addr = INADDR_ANY;
+
+    return true;
 }
 
-bool Server::start(uint16_t _port)
-{
-    int sockfd, newsockfd;
-    uint16_t portno;
-    socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-
-    // create a socket
-    // socket(int domain, int type, int protocol)
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
-    // clear address structure
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-
-    portno = _port;
-
-    /* setup the host_addr structure for use in bind call */
-    // server byte order
-    serv_addr.sin_family = AF_INET;
-
-    // automatically be filled with current host's IP address
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-    // convert short integer value for port must be converted into network byte order
-    serv_addr.sin_port = htons(portno);
-
-    // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
-    // bind() passes file descriptor, the address structure,
-    // and the length of the address structure
-    // This bind() call will bind  the socket to the current IP address on port, portno
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
-
-    // This listen() call tells the socket to listen to the incoming connections.
-    // The listen() function places all incoming connection into a backlog queue
-    // until accept() call accepts the connection.
-    // Here, we set the maximum size for the backlog queue to 5.
-    listen(sockfd, 5);
-
-    // The accept() call actually accepts an incoming connection
-    clilen = sizeof(cli_addr);
-
-    // This accept() function will write the connecting client's address info
-    // into the the address structure and the size of that structure is clilen.
-    // The accept() returns a new socket file descriptor for the accepted connection.
-    // So, the original socket file descriptor can continue to be used
-    // for accepting new connections while the new socker file descriptor is used for
-    // communicating with the connected client.
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-    if (newsockfd < 0)
-        error("ERROR on accept");
-
-    printf("server: got connection from %s port %d\n",
-           inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-
-    // This send() function sends the 13 bytes of the string to the new socket
-    send(newsockfd, "Hello, world!\n", 13, 0);
-
-    bzero(buffer, 256);
-
-    n = read(newsockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("Here is the message: %s\n", buffer);
-
-    close(newsockfd);
-    close(sockfd);
-    return 0;
+void Server::stop() {
+    close(this->sockfd);
 }
